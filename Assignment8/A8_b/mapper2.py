929c31c4345e5cd66052a3eedb24b8d7bfc920ad
VikasRajput2002 <93874584+VikasRajput2002@users.noreply.github.com>
2021-12-05 20:51:25 +0530

    Add files via upload

---
+++ Assignment8/A8_b/mapper2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import sys
+import re
+
+for line in sys.stdin:
+    line = line.strip().lower()
+    words = re.findall(r'\w+', line)
+    #line = re.sub('[^\w\d\s\-]+', '', line)
+    #words = line.split()
+    for word in words:
+        print(f'{word},1')
